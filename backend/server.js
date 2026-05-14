3f44f7d5cc75403227f51bd670a7ba9786988c3e
gtakamoto555-oss <gtakamoto555@gmail.com>
2026-05-15 01:57:49 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -137,7 +137,7 @@ const __dirname = path.dirname(__filename);
 
 // --- Reactフロントエンドの配信（本番環境用） ---
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
+app.app.use((req, res) => { (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
